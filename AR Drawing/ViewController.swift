55418c3e3b8320883ff84aede098d4f57423dca5
Machine Horizon <cudoskylab@machines-macbook-pro.local>
2018-02-26 14:31:20 +0100

    Initial Commit

---
+++ AR Drawing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AR Drawing
+//
+//  Created by Machine Horizon on 26/02/2018.
+//  Copyright © 2018 Machine Horizon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
